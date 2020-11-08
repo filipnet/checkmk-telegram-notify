e39f6b964bf59ffab4329b311f8e9148f8700f0c
filipnet <benedikt.filip@filipnet.de>
2020-11-08 22:35:38 +0100

    update

---
+++ README.md
@@ -0,0 +1,56 @@
+# Check_MK Telegram notification
+Telegram has long been one of my real-time communication media. It is obvious to output monitoring messages for server and network components as alarm messages. There are several scripts for this on the internet, but most of them are written in Python, many of them have problems with Python3 and its libraries. Instead of spending hours and hours with Python, I decided to use a scripting language I know and write a Linux Bash script for it.
+
+The following Script is for Check_MK, I have used it exclusively with the RAW version 1.6.0_p18.
+
+## INSTALLATION
+Change to your Check_MK site user
+```
+su - mysite
+```
+
+Change to the notification directory
+```
+cd ~/local/share/check_mk/notifications/
+```
+
+Download the Telegram notify script from Git repository
+
+```
+wget https://github.com/filipnet/telegram/blob/main/check_mk_telegram-notify.sh
+wget https://github.com/filipnet/telegram/blob/main/config.xml.sample
+```
+or 
+```
+git clone https://github.com/filipnet/telegram.git
+cd telegram
+cp check_mk_telegram-notify.sh /opt/omd/sites/mysite/local/share/check_mk/notifications/
+cp config.xml.sample /opt/omd/sites/mysite/local/share/check_mk/notifications/
+```
+
+Adjusting the config.xml
+```
+cd /opt/omd/sites/mysite/local/share/check_mk/notifications/
+cp config.xml.sample config.xml
+```
+
+Inside your API Token and Chat/Group-ID
+```
+<telegram_api_token>TELEGRAM_API_TOKEN_WITHOUT_BOT_PREFIX</telegram_api_token>
+<telegram_chat_id>TELEGRAM_GROUP_OR_CHAT-ID</telegram_chat_id>
+```
+
+Give the script execution permissions
+```
+chmod +x check_mk_telegram-notify.sh
+```
+
+## CHECK_MK CONFIGURATION
+Now you can create your own alarm rules in Check_MK.
+
+```WATO → Notifications → New Rule → Notification Method → Push Notification (using Telegram)```
+
+No further settings are required for this.
+
+## LICENSE
+telegram and all individual scripts are under the BSD 3-Clause license unless explicitly noted otherwise. Please refer to the LICENSE
---
+++ check_mk_telegram-notify.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Push Notification (using Telegram)
+#
+# Script Name   : check_mk_telegram-notify.sh
+# Description   : Send Check_MK notifications by Telegram
+# Author        : https://github.com/filipnet/telegram
+# License       : BSD 3-Clause "New" or "Revised" License
+# ======================================================================================
+
+CONFIGFILE="${BASH_SOURCE%/*}/config.xml"
+# Telegram API Token
+# Find telegram bot named "@botfarther", type /mybots, select your bot and select "API Token" to see your current token
+TOKEN=$(xmllint --xpath 'string(/config/telegram_api_token)' $CONFIGFILE)
+# Telegram Chat-ID or Group-ID
+# Open "https://api.telegram.org/bot<YOUR_TOKEN>/getUpdates" inside your Browser and send a HELLO to your bot, refresh side
+CHAT_ID=$(xmllint --xpath 'string(/config/telegram_chat_id)' $CONFIGFILE)
+
+# Write Check_MK output to a temporary file, delete depricated macros and create variable OUTPUT
+env | grep NOTIFY_ | grep -v "This macro is deprecated" | sort > $OMD_ROOT/tmp/telegram.out
+OUTPUT=$OMD_ROOT/tmp/telegram.out
+
+# Read OUTPUT variable and create some more variables for every text-part you want to use afterwards
+HOSTNAME=$(grep NOTIFY_HOSTNAME $OUTPUT | cut -d'=' -f2)
+HOSTALIAS=$(grep NOTIFY_HOSTALIAS $OUTPUT | cut -d'=' -f2)
+WHAT=$(grep NOTIFY_WHAT $OUTPUT | cut -d'=' -f2)
+NOTIFICATIONTYPE=$(grep NOTIFY_NOTIFICATIONTYPE $OUTPUT | cut -d'=' -f2)
+SERVICEDESC=$(grep NOTIFY_SERVICEDESC $OUTPUT | cut -d'=' -f2)
+SERVICEOUTPUT=$(grep NOTIFY_SERVICEOUTPUT $OUTPUT | cut -d'=' -f2)
+HOSTOUTPUT=$(grep NOTIFY_HOSTOUTPUT $OUTPUT | cut -d'=' -f2)
+PREVIOUSHOSTHARDSHORTSTATE=$(grep NOTIFY_PREVIOUSHOSTHARDSHORTSTATE $OUTPUT | cut -d'=' -f2)
+HOSTSHORTSTATE=$(grep NOTIFY_HOSTSHORTSTATE $OUTPUT | cut -d'=' -f2)
+PREVIOUSSERVICEHARDSHORTSTATE=$(grep NOTIFY_PREVIOUSSERVICEHARDSHORTSTATE $OUTPUT | cut -d'=' -f2)
+SERVICESHORTSTATE=$(grep NOTIFY_SERVICESHORTSTATE $OUTPUT | cut -d'=' -f2)
+SHORTDATETIME=$(grep NOTIFY_SHORTDATETIME $OUTPUT | cut -d'=' -f2)
+HOST_ADDRESS_4=$(grep NOTIFY_HOST_ADDRESS_4 $OUTPUT | cut -d'=' -f2)
+HOST_ADDRESS_6=$(grep NOTIFY_HOST_ADDRESS_6 $OUTPUT | cut -d'=' -f2)
+
+# Create a MESSAGE variable to send to your Telegram bot
+MESSAGE="$HOSTNAME ($HOSTALIAS)%0A"
+MESSAGE+="$WHAT $NOTIFICATIONTYPE%0A%0A"
+if [ $WHAT == "SERVICE" ]; then
+        MESSAGE+="$SERVICEDESC%0A"
+        MESSAGE+="State changed from $PREVIOUSSERVICEHARDSHORTSTATE to $SERVICESHORTSTATE%0A"
+        MESSAGE+="$SERVICEOUTPUT%0A"
+else
+        MESSAGE+="State changed from $PREVIOUSHOSTHARDSHORTSTATE to $HOSTSHORTSTATE%0A"
+        MESSAGE+="$HOSTOUTPUT%0A"
+fi
+MESSAGE+="%0AIPv4: $HOST_ADDRESS_4 %0AIPv6: $HOST_ADDRESS_6%0A"
+MESSAGE+="$SHORTDATETIME"
+
+# Send message to Telegram bot
+curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" -d chat_id=$CHAT_ID -d text="$MESSAGE" >> /dev/null
+
+# End of script
+exit 0
